f8dca01e1066881f60474014645249d1261ba960
Lawson Lewis <lawson@kurrawong.ai>
2024-06-07 10:57:12 +1000

    Split into producer and consumer
    new dockerfile
    
    add producer
    
    split complete
    
    update readme
    
    add bulma css

--- src/consumer.py
+++ src/consumer.py
@@ -6,13 +6,12 @@ from pathlib import Path
 from textwrap import dedent
 from uuid import uuid4
 
-import requests
 import uvicorn
 from azure.servicebus import ServiceBusClient, ServiceBusMessage, ServiceBusReceiver
 from dotenv import load_dotenv
 from fastapi import FastAPI, Form
 from fastapi.requests import Request
-from fastapi.responses import HTMLResponse, Response
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from jinja2 import Template
 from rdf_delta import DeltaClient
@@ -22,7 +21,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter("%(levelname)s:  %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -31,16 +30,13 @@ logger.addHandler(handler)
 
 load_dotenv()
 
-sparql_endpoint = os.getenv("SPARQL_ENDPOINT")
-sparql_update_endpoint = os.getenv("SPARQL_UPDATE_ENDPOINT")
+broker_topic = os.getenv("BROKER_TOPIC", "")
+broker_endpoint = os.getenv("BROKER_ENDPOINT", "")
+broker_subscription = os.getenv("BROKER_SUBSCRIPTION", "")
+broker_connection_str = os.getenv("BROKER_CONNECTION_STR", "")
 
-broker_topic = os.getenv("BROKER_TOPIC")
-broker_endpoint = os.getenv("BROKER_ENDPOINT")
-broker_subscription = os.getenv("BROKER_SUBSCRIPTION")
-broker_connection_str = os.getenv("BROKER_CONNECTION_STR") or input("broker_connection_str: ")
-
-rdfdelta_endpoint = os.getenv("RDFDELTA_ENDPOINT")
-rdfdelta_datasource = os.getenv("RDFDELTA_DATASOURCE")
+rdfdelta_endpoint = os.getenv("RDFDELTA_ENDPOINT", "")
+rdfdelta_datasource = os.getenv("RDFDELTA_DATASOURCE", "")
 
 app = FastAPI()
 templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
@@ -93,59 +89,13 @@ async def index(request: Request):
             "broker_endpoint": broker_endpoint,
             "broker_topic": broker_topic,
             "broker_subscription": broker_subscription,
-            "broker_connection_str": broker_connection_str[:25] + "..." + broker_connection_str[-12:],
+            "broker_connection_str": broker_connection_str[:25]
+            + "..."
+            + broker_connection_str[-12:],
             "rdfdelta_endpoint": rdfdelta_endpoint,
             "rdfdelta_datasource": rdfdelta_datasource,
-            "sparql_endpoint": sparql_endpoint,
-            "sparql_update_endpoint": sparql_update_endpoint,
         },
     )
-
-
-@app.post("/update")
-async def sparql_update(
-    sparql_update_endpoint: str = Form(...),
-    sparql_update_query: str = Form(...),
-):
-    response = requests.post(
-        sparql_update_endpoint, data={"update": sparql_update_query}
-    )
-    response.raise_for_status()
-    logger.debug(response)
-    return HTMLResponse(response.text)
-
-
-@app.post("/query")
-async def sparql_select(
-    sparql_endpoint: str = Form(...),
-    sparql_query: str = Form(...),
-):
-    headers = {
-        "Accept": "text/csv",
-        "Content-Type": "application/sparql-query",
-    }
-    response = requests.post(sparql_endpoint, data=sparql_query, headers=headers)
-    response.raise_for_status()
-    query_response = response.text
-    logger.debug(query_response)
-    content = html.escape(query_response).replace("\n", "<br>")
-    return Response(content=content, media_type="text/csv")
-
-
-@app.post("/produce")
-async def produce(
-    request: Request,
-    broker_endpoint: str = Form(...),
-    broker_topic: str = Form(...),
-    subject: str = Form(...),
-    body: str = Form(...),
-):
-    servicebus_client = ServiceBusClient.from_connection_string(broker_connection_str)
-    sender = servicebus_client.get_topic_sender(topic_name=broker_topic)
-    sender.send_messages(ServiceBusMessage(subject=subject, body=body))
-    servicebus_client.close()
-    message = "Message sent successfully to Azure Service Bus"
-    return HTMLResponse(content=message)
 
 
 @app.post("/peek")
@@ -184,6 +134,7 @@ async def consume(
         topic_name=broker_topic, subscription_name=broker_subscription
     )
     messages = reciever.receive_messages(max_message_count=1, max_wait_time=1)
+    logger.debug(messages)
     if messages:
         msg = messages[0]
         process_message(msg, reciever)
@@ -210,4 +161,19 @@ async def get_log(
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    if not broker_connection_str:
+        logger.error("BROKER_CONNECTION_STR not set", "")
+        exit()
+    env_vars = "\n".join(
+        [
+            "ENV VARS",
+            broker_topic,
+            broker_endpoint,
+            broker_subscription,
+            broker_connection_str,
+            rdfdelta_endpoint,
+            rdfdelta_datasource,
+        ]
+    )
+    logger.debug(env_vars)
+    uvicorn.run("consumer:app", host="127.0.0.1", port=8080, reload=True)
