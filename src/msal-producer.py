b2818b87ea7fda8bcd285841cd434d800fda9397
Lawson Lewis <lawson@kurrawong.ai>
2024-07-18 17:23:19 +1000

    udpates

--- src/msal-producer.py
+++ src/msal-producer.py
@@ -1,12 +1,13 @@
 import logging
-from html import escape
 import os
 import sys
 from enum import Enum
+from html import escape
 from pathlib import Path
 
 import msal
 import requests
+import starlette.status as status
 import uvicorn
 from azure.identity import ClientSecretCredential
 from azure.servicebus import ServiceBusClient, ServiceBusMessage, ServiceBusReceiver
@@ -18,6 +19,7 @@ from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.security import SecurityScopes
 from fastapi.templating import Jinja2Templates
 from jinja2 import Template
+from requests.exceptions import ConnectionError, HTTPError
 
 logger = logging.Logger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -30,8 +32,7 @@ load_dotenv()
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 tenant_id = os.environ.get("TENANT_ID")
-admin_app_role_id = os.environ.get("ADMIN_APP_ROLE_ID")
-host_domain = os.environ.get("HOST_DOMAIN")
+admin_scope = os.environ.get("ADMIN_SCOPE")
 
 # Use MSAL to authenticate the user logging into the application
 msal_app = msal.ConfidentialClientApplication(
@@ -39,11 +40,12 @@ msal_app = msal.ConfidentialClientApplication(
     authority="https://login.microsoftonline.com/" + tenant_id,
 )
 
-session: dict = dict()
+users: dict = dict()
+auth_flows: dict = dict()
 
 
-class AppRole(Enum):
-    EventHarnessAdmin = admin_app_role_id
+class Scopes(Enum):
+    ReadWriteAll = admin_scope
 
 
 class AuthMethod(Enum):
@@ -72,58 +74,65 @@ async def get_sb_client(
     return sb_client
 
 
-async def get_token() -> str | None:
-    scopes = ["AppRoleAssignment.ReadWrite.All"]
-    accounts = msal_app.get_accounts()
-    result = None
-    if accounts:
-        chosen = accounts[0]  # default to first account if there are multiple
-        result = msal_app.acquire_token_silent(scopes=scopes, account=chosen)
-    if not result:
-        flow = msal_app.initiate_auth_code_flow(
-            scopes=scopes, redirect_uri=f"{host_domain}/token"
-        )
-        if "error" in flow:
-            logging.DEBUG(flow.get("error"))
-        session["auth_flow"] = flow
-        return None
-    token = result["access_token"]
-    return token
+@app.get("/login")
+async def login(request: Request):
+    scopes = ["User.Read"]
+    # accounts = msal_app.get_accounts(username=username)
+    # if accounts:
+    #     chosen = accounts[0]  # default to first account if there are multiple
+    #     result = msal_app.acquire_token_silent(scopes=scopes, account=chosen)
+    # if not result:
+    flow = msal_app.initiate_auth_code_flow(
+        scopes=scopes,
+        redirect_uri=str(request.base_url) + "token",
+        response_mode="form_post",
+    )
+    if "error" in flow:
+        return JSONResponse(status_code=500, content={"error": flow.get("error")})
+    auth_flows[flow["state"]] = flow
+    return RedirectResponse(flow["auth_uri"])
 
 
-@app.get("/token", response_class=RedirectResponse)
-async def token(request: Request):
+@app.post("/token", response_class=RedirectResponse)
+async def token(
+    request: Request,
+):
     token = None
+    session_id = request.cookies.get("JSESSIONID")
+    form = await request.form()
+    state = form.get("state")
     try:
         result = msal_app.acquire_token_by_auth_code_flow(
-            session.get("auth_flow"), dict(request.query_params)
+            auth_flows.get(state), dict(form)
         )
-        if "access_token" in result:
-            token = result["access_token"]
-        else:
+        token = result.get("access_token")
+        if not token:
             logger.debug(result.get("error"))
     except ValueError as e:
         logger.debug(e)
         pass
-    return RedirectResponse("/")
+    users[session_id] = result
+    return RedirectResponse("/", status_code=status.HTTP_302_FOUND)
 
 
-async def get_username(token: str) -> str:
-    if not token:
+async def get_user(session_id: str):
+    return users.get(session_id)
+
+
+async def is_authorized(user: dict, scopes: list[str]) -> bool:
+    role_claims = user["id_token_claims"].get("roles")
+    if not role_claims:
+        return False
+    for scope in scopes:
+        if scope not in role_claims:
+            return False
+    return True
+
+
+async def get_approle_ids(access_token: str) -> list[str]:
+    if not access_token:
         return None
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(
-        "https://graph.microsoft.com/v1.0/me",
-        headers=headers,
-    )
-    response.raise_for_status()
-    return response.json().get("displayName")
-
-
-async def get_approle_ids(token: str) -> list[str]:
-    if not token:
-        return None
-    headers = {"Authorization": f"Bearer {token}"}
+    headers = {"Authorization": f"Bearer {access_token}"}
     response = requests.get(
         "https://graph.microsoft.com/v1.0/me/appRoleAssignments",
         headers=headers,
@@ -133,67 +142,34 @@ async def get_approle_ids(token: str) -> list[str]:
     return ids
 
 
-async def is_authorized(
-    scopes: SecurityScopes, token: str = Depends(get_token)
-) -> bool:
-    """Check if a user is authorized against a set of scopes
-
-    :param scopes: A list of scopes (App Role ID's)
-    :param token: an Oauth2 token
-    :return: True or False
-    """
-    # user is not authenticated
-    if not token:
-        return False
-    # user is authenticated and no scopes required
-    if not scopes:
-        return True
-    approle_ids = await get_approle_ids(token)
-    # scopes are required but no approles are assigned to the user
-    if not approle_ids:
-        return False
-    for scope in scopes.scopes:
-        # required scope is not assigned to the user
-        if scope not in approle_ids:
-            return False
-    # all required scopes are assigned to the user
-    return True
-
-
 @app.get("/")
 async def index(
     request: Request,
-    token: str = Depends(get_token),
-    authorized: bool = Security(
-        is_authorized, scopes=[AppRole.EventHarnessAdmin.value]
-    ),
 ):
-    authenticated = True if token else False
-    auth_uri = session.get("auth_flow").get("auth_uri")
-    username = await get_username(token)
-    if authenticated and not authorized:
-        return HTMLResponse(content="error: Unauthorized access", status_code=401)
+    session_id = request.cookies.get("JSESSIONID")
+    user = await get_user(session_id)
+    if user:
+        authorized = await is_authorized(user, [Scopes.ReadWriteAll.value])
+        if not authorized:
+            return JSONResponse(
+                status_code=401, content={"error": "Unauthorized access"}
+            )
     return templates.TemplateResponse(
         "producer-index.html",
         {
             "request": request,
-            "authenticated": authenticated,
-            "auth_uri": auth_uri,
-            "username": username,
+            "user": user,
             "client_id": client_id[:9] + "..." + client_id[-9:],
             "client_secret": client_secret[:9] + "..." + client_secret[-9:],
             "tenant_id": tenant_id[:9] + "..." + tenant_id[-9:],
-            "admin_app_role_id": admin_app_role_id,
-            "host_domain": host_domain
+            "admin_scope": admin_scope
         },
     )
 
 
 @app.post("/sb-msg")
 async def sb_msg(
-    authorized: bool = Security(
-        is_authorized, scopes=[AppRole.EventHarnessAdmin.value]
-    ),
+    request: Request,
     sb_auth_method: AuthMethod = Form(),
     sb_namespace: str = Form(default=""),
     sb_conn_str: str = Form(default=""),
@@ -201,8 +177,14 @@ async def sb_msg(
     msg_subject: str = Form(),
     msg_body: str = Form(),
 ):
-    if not authorized:
-        return HTMLResponse(content="error: Unauthorized access", status_code=401)
+    session_id = request.cookies.get("JSESSIONID")
+    user = await get_user(session_id)
+    if user:
+        authorized = await is_authorized(user, [Scopes.ReadWriteAll.value])
+        if not authorized:
+            return JSONResponse(
+                status_code=401, content={"error": "Unauthorized access"}
+            )
     if sb_auth_method == AuthMethod.AAD and not sb_namespace:
         return HTMLResponse(
             status_code=400, content="Namespace required for Azure AD authentication"
@@ -238,21 +220,39 @@ async def sb_msg(
 
 @app.post("/query")
 async def query(
-    authorized: bool = Security(
-        is_authorized, scopes=[AppRole.EventHarnessAdmin.value]
-    ),
+    request: Request,
     sparql_endpoint: str = Form(),
     query_str: str = Form(),
 ):
-    if not authorized:
-        return HTMLResponse(content="error: Unauthorized access", status_code=401)
-    response = requests.get(
-        sparql_endpoint,
-        params={"query": query_str},
-        headers={"Accept": "application/json"},
+    session_id = request.cookies.get("JSESSIONID")
+    user = await get_user(session_id)
+    if user:
+        authorized = await is_authorized(user, [Scopes.ReadWriteAll.value])
+        if not authorized:
+            return JSONResponse(
+                status_code=401, content={"error": "Unauthorized access"}
+            )
+    try:
+        response = requests.get(
+            sparql_endpoint,
+            params={"query": query_str},
+            headers={"Accept": "application/json"},
+        )
+        response.raise_for_status()
+    except ConnectionError as e:
+        return JSONResponse(
+            status_code=500,
+            content={"error": str(type(e)), "message": str(e.args[0])},
+        )
+    except HTTPError as e:
+        return JSONResponse(
+            status_code=response.status_code,
+            content={"error": str(type(e)), "message": str(e.args[0])},
+        )
+
+    return JSONResponse(
+        status_code=response.status_code, content=response.json()
     )
-    response.raise_for_status()
-    return JSONResponse(status_code=200, content=response.json())
 
 
 if __name__ == "__main__":
